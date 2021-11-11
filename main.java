9ec18754da7bfd983bb52dca4cf228fe9e8d0d20
Brejnev1917 <91326516+Brejnev1917@users.noreply.github.com>
2021-11-11 15:59:33 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int tiket = 10_000;
+        int miles = (tiket / 20);
+        int total = miles;
+        System.out.println(total);
+    }    
+
+}
+
